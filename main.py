754df3bba6820003d70ccf96296a109678cd0ed9
JunEunPark <pppp662667@gmail.com>
2025-08-14 12:41:51 +0900

    modify merge_sort

--- main.py
+++ main.py
@@ -36,7 +36,36 @@ def insertion_sort(arr):
     
 
 def merge_sort(arr, depth=0):
-    pass
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left, depth + 1)
+        merge_sort(right, depth + 1)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+    if depth == 0:
+        print(arr)
 
 def quick_sort(arr):
     def _quick_sort(a, low, high):
